d47da000334d29451c9be692fd68972be9c21053
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-09-23 03:15:08 +0600

    �

---
+++ Day - 02/script.js
@@ -0,0 +1,13 @@
+var celsius = document.getElementById("celsius").value;
+var fahrenheit = document.getElementById('fahrenheit').value;
+
+function celTofar() {
+    let output = (celsius);
+    // fahrenheit = parseFloat(output.toFixed(2));
+    console.log(output);
+}
+
+function sayHello() {
+    // alert("Hello World!");
+    console.log("Hello World!");
+}
