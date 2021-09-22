93642ba512622b9ca1c37c5470f17baf437fc39d
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-09-23 04:12:06 +0600

    ✅

--- Day - 02/script.js
+++ Day - 02/script.js
@@ -1,13 +1,18 @@
-var celsius = document.getElementById("celsius").value;
-var fahrenheit = document.getElementById('fahrenheit').value;
+let celsius = document.getElementById("celsius"); //getting the value of celcius input field
+let fahrenheit = document.getElementById('fahrenheit'); //getting the value of fahrenheit input field
 
-function celTofar() {
-    let output = (celsius);
-    // fahrenheit = parseFloat(output.toFixed(2));
-    console.log(output);
+// this function will be called when user will input a value in the celsius input field
+function celToFar(){
+    let cel = parseFloat(celsius.value);
+    let output = (cel * 9/5) + 32;
+    fahrenheit.value = output.toFixed(2);
 }
 
-function sayHello() {
-    // alert("Hello World!");
-    console.log("Hello World!");
+
+// this function will be called when user will input a value in the fahrenheit input field
+function farToCel(){
+    let far = parseFloat(fahrenheit.value);
+    let output = (far - 32) * 5/9;
+    celsius.value = output.toFixed(2);
+    console.log(output.toFixed(2));
 }
