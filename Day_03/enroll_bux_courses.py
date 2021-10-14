2756f62dbf3d3590c8c842b69a13a8922ff9328f
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-10-14 06:29:23 +0600

    bux automation

---
+++ Day_03/enroll_bux_courses.py
@@ -0,0 +1,40 @@
+# 'E:/GitHub/365-Days-of-Coding/Day_03/chromedriver.exe'
+from selenium import webdriver
+import os
+import webbrowser
+import time
+
+os.environ['PATH'] += r"E:/GitHub/365-Days-of-Coding/Day_03"
+
+# User info:
+bux_email = input('Enter your email address for buX: ')
+bux_password = input('Password: ')
+courses = list(input('Enter courses code you want to enroll (separated by comma):').split(','))
+
+driver = webdriver.Chrome()
+
+def buX_login(email, password):
+    driver.get('https://bux.bracu.ac.bd/login')
+    driver.fullscreen_window()
+    emailForm = driver.find_element_by_xpath('//*[@id="login-email"]')
+    emailForm.send_keys(email)
+    passwordForm = driver.find_element_by_xpath('//*[@id="login-password"]')
+    passwordForm.send_keys(password)
+    loginButton = driver.find_element_by_xpath('//*[@id="login"]/button')
+    loginButton.click()
+    time.sleep(10)
+    print('[] - Login Successfull ')
+
+def enroll_course(course_code):
+    driver.fullscreen_window()
+    driver.get(f'https://bux.bracu.ac.bd/courses/course-v1:buX+{course_code.upper()}+2021_Fall/about')
+    enroll_Course = driver.find_element_by_xpath('//*[@id="content"]/section/header[2]/div/div[2]/h1/div/a')
+    enroll_Course.click()
+    time.sleep(10)
+    
+    
+buX_login(bux_email,bux_password)
+for i in courses:
+    enroll_course(i.strip())
+    
+driver.quit()
